062c8e9c53e409ce1e7d35fea94fa6af4ee8742b
Mattia-Giacobelli <giacobelli.mattia12@gmail.com>
2025-11-24 12:06:01 +0100

    return server del mio blog

---
+++ server.js
@@ -0,0 +1,13 @@
+const { log } = require('console')
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+    res.send('Server del mio blog')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on http://localhost:${port}`);
+})
